46cf3dff68106da7d6a53b3a8119ac4dedb950b5
Rahul <pratibhaayanrahul@gmail.com>
2022-02-03 20:41:58 +0530

    update form

--- forms.py
+++ forms.py
@@ -9,7 +9,7 @@ from wtforms.fields.html5 import EmailField
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
